b67f76f429da5a560f8a6398c5dce71114307fc4
guilherme-win <gui.dani13@gmail.com>
2020-12-08 14:24:11 -0300

    q12

--- Atividades/Semana_02/q12/Desconto.java
+++ Atividades/Semana_02/q12/Desconto.java
@@ -1,5 +1,11 @@
-package q12;
-
 public class Desconto {
+
+    double valorProduto;
+    int desc;
+
+    public double calcula(){
+        double valorCalculado = valorProduto - ((valorProduto/100)*desc);
+        return valorCalculado;
+    }
     
 }
--- Atividades/Semana_02/q12/TestaDesconto.java
+++ Atividades/Semana_02/q12/TestaDesconto.java
@@ -1,5 +1,10 @@
-package q12;
-
 public class TestaDesconto {
-    
+    public static void main(String[] args) {
+        Desconto d = new Desconto();
+
+        d.valorProduto = 120;
+        d.desc = 10;
+
+        System.out.println(d.calcula());
+    }    
 }
